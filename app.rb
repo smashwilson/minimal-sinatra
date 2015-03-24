6e03c77fd8b16062b46219fd3cb43adc33242ea0
Ash Wilson <smashwilson@gmail.com>
2015-03-24 09:29:24 -0400

    Yep

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@
 require 'sinatra'
 
 # Let's add a comment I can pointlessly increment to manufacture "work".
-# 0005
+# 0006
 
 get '/' do
   "I'm currently running revision #{`git rev-parse --short HEAD`}"
